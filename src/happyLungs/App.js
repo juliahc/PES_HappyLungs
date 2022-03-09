77d191b678b9df96a09883b37803b9dc0565e0c4
alexs <alejandro.soriano@estudiantat.upc.edu>
2022-03-09 17:34:57 +0100

    Test

--- src/happyLungs/App.js
+++ src/happyLungs/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola</Text>
+      <Text>Es la rama de Alex</Text>
       <StatusBar style="auto" />
     </View>
   );
